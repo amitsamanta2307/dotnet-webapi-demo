46a6b08169ff7bc068961f3aa1430fc5267018ea
Amit Samanta <amit.samanta@outlook.com>
2019-11-03 21:24:58 +0530

    Access the data from the SQL Server using the ApiController

---
+++ WebAPIDemo/WebAPIDemo/Controllers/EmployeesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIDemo.Data;
+
+namespace WebAPIDemo.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBEntities context = new EmployeeDBEntities())
+            {
+                return context.Employees.ToList();
+            }
+        }
+
+        public Employee Get(int id)
+        {
+            using (EmployeeDBEntities context = new EmployeeDBEntities())
+            {
+                return context.Employees.SingleOrDefault(ee => ee.Id == id);
+            }
+        }
+    }
+}
