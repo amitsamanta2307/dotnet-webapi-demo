a452d3a936201aebd65c180c17d4966ed7079ff0
Amit Samanta <amit.samanta@outlook.com>
2019-11-03 21:49:55 +0530

    Change the serializer settings of the formatter to properly indent and use the camel case instead of the pascal case for property names

--- WebAPIDemo/WebAPIDemo/App_Start/WebApiConfig.cs
+++ WebAPIDemo/WebAPIDemo/App_Start/WebApiConfig.cs
@@ -19,6 +19,10 @@ namespace WebAPIDemo
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // Adding the media type formatters
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
         }
     }
 }
